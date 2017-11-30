d5f96a10b7b1255fa6c0c24a497e33c0a93622b9
Keely Meyers <keelym@umich.edu>
2017-11-29 22:15:27 -0500

    created caching setup

--- final.py
+++ final.py
@@ -1,2 +1,33 @@
 import requests 
 import json
+
+
+CACHE_FNAME = "cached_data.json"
+# Put the rest of your caching setup here:
+
+try:
+    cache_file = open(CACHE_FNAME,'r')
+    cache_contents = cache_file.read()
+    cache_file.close()
+    CACHE_DICTION = json.loads(cache_contents)
+except:
+    CACHE_DICTION = {}
+
+def requestURL(baseurl, params = {}):
+    req = requests.Request(method = 'GET', url = baseurl, params = params)
+    prepped = req.prepare()
+    return prepped.url
+
+def get_with_caching(base_url, params_diction, cache_diction, cache_fname):
+    full_url = requestURL(base_url, params_diction)
+    if full_url in cache_diction:
+        #logging.info("retrieving cached result for " + full_url)
+        return cache_diction[full_url]
+    else:
+        response = requests.get(base_url, params=params_diction)
+        #logging.info("adding cached result for " + full_url)
+        cache_diction[full_url] = response.text
+        fobj = open(cache_fname, "w")
+        pickle.dump(cache_diction, fobj)
+        fobj.close()
+        return response.text
